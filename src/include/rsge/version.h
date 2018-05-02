8f6b38a4bde60cc5360ff4f2567f8bb13e4e548a
SpaceboyRoss01 <spaceboyross@yandex.com>
2018-05-02 14:45:39 -0700

    Update version.h

--- src/include/rsge/version.h
+++ src/include/rsge/version.h
@@ -1,6 +1,11 @@
 #ifndef __RSGE_VERSION_H_
 #define __RSGE_VERSION_H_ 1
 
+/** \file src/include/rsge/version.h
+ * \brief Version header
+ * \author Spaceboy Ross
+ */
+
 #define RSGE_VERSION_MAJOR 0
 #define RSGE_VERSION_MINOR 1
 #define RSGE_VERSION_RELEASE 0
