81b4c9be76389c2a1bfb550eb183f1f45678bbd5
SpaceboyRoss01 <spaceboyross@yandex.com>
2018-05-02 14:47:00 -0700

    Update window.h

--- src/include/rsge/window.h
+++ src/include/rsge/window.h
@@ -1,14 +1,19 @@
 #ifndef __RSGE_WINDOW_H_
 #define __RSGE_WINDOW_H_ 1
 
+/** \file src/include/rsge/window.h
+ * \brief Window stuff.
+ * \author Spaceboy Ross
+ */
+
 #include <rsge/gfx/surface.h>
 #include <rsge/error.h>
 
 /**
  * \fn rsge_error_e rsge_window_seticon_fromSurfaces(rsge_surface_t** surfaces,int surfaceCount)
  * \brief Sets the window icon to an array of surfaces.
- * \param surfaces The surfaces to use.
- * \param surfaceCount The number of surfaces.
+ * \param[in] surfaces The surfaces to use.
+ * \param[in] surfaceCount The number of surfaces.
  * \return An error code.
  */
 rsge_error_e rsge_window_seticon_fromSurfaces(rsge_surface_t** surfaces,int surfaceCount);
@@ -16,8 +21,8 @@ rsge_error_e rsge_window_seticon_fromSurfaces(rsge_surface_t** surfaces,int surf
 /**
  * \fn rsge_error_e rsge_window_seticon_fromFiles(char** paths,int pathCount)
  * \brief Sets the window icon.
- * \param paths The paths to the assets to use.
- * \param pathCount The number of paths.
+ * \param[in] paths The paths to the assets to use.
+ * \param[in] pathCount The number of paths.
  * \return An error code.
  */
 rsge_error_e rsge_window_seticon_fromFiles(char** paths,int pathCount);
