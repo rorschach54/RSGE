feb7452a574b03b38375161528d9aab8c9e77324
Spaceboy ross <spaceboyross@yandex.com>
2018-05-07 21:48:32 +0000

    WIP .obj model loader

--- src/library/gfx/model/obj.c
+++ src/library/gfx/model/obj.c
@@ -1,6 +1,7 @@
 #include <rsge/gfx/model/obj.h>
 #include <rsge/gfx/objects/texture.h>
 #include <rsge/gfx/colors.h>
+#include <rsge/gfx/mesh.h>
 #include <rsge/assets.h>
 #include <log.h>
 #include <linmath.h>
@@ -84,6 +85,10 @@ static rsge_error_e rsge_model_obj_readFloat(char* line,float* val) {
     return RSGE_ERROR_NONE;
 }
 
+static rsge_error_e rsge_model_obj_readMaterials(char* path,char* baseDir,list_t** materials) {
+    return RSGE_ERROR_NONE;
+}
+
 static rsge_error_e rsge_model_obj_readTexture(char* line,char* baseDir,rsge_obj_texture_t* texture) {
     while(line[0] == ' ') line++;
     char** split;
@@ -99,6 +104,91 @@ static rsge_error_e rsge_model_obj_readTexture(char* line,char* baseDir,rsge_obj
 rsge_error_e rsge_model_obj_fromFile(list_t** objects,char* path) {
     *objects = list_new();
     if(!objects) return RSGE_ERROR_MALLOC;
+    
+    list_t* asdf = list_new();
+    if(!asdf) return RSGE_ERROR_MALLOC;
+    char* line = "";
+    list_t* verts = list_new();
+    if(!verts) return RSGE_ERROR_MALLOC;
+    list_t* normals = list_new();
+    if(!normals) return RSGE_ERROR_MALLOC;
+    list_t* uvs = list_new();
+    if(!uvs) return RSGE_ERROR_MALLOC;
+    list_t* materials = list_new();
+    if(!materials) return RSGE_ERROR_MALLOC;
+    
+    char* currentObjectName = "";
+    size_t baseDirectorySize = strlen(path);
+    while(baseDirectorySize > 0 && path[baseDirectorySize--] == '/') break;
+    while(path[baseDirectorySize--] == '/');
+    char* baseDirectory = malloc(baseDirectorySize);
+    if(!baseDirectory) return RSGE_ERROR_MALLOC;
+    strncpy(baseDirectory,path,baseDirectorySize);
+    
+    rsge_material_t currentMaterial;
+    char* data;
+    size_t datasz;
+    rsge_error_e err = rsge_asset_read(path,&data,&datasz);
+    if(err != RSGE_ERROR_NONE) return err;
+    
+    line = data;
+    while(line) {
+        char* nextLine = strchr(line,'\n');
+        if(nextLine) *nextLine = '\0';
+        
+        if(strlen(line) > 1) {
+            char** split;
+            size_t split_count;
+            err = rsge_model_obj_splitStr(line,' ',&split,&split_count);
+            if(err != RSGE_ERROR_NONE) return err;
+            char* keyword = split[0];
+            
+            if(!!strcmp(keyword,"#")) {
+                if(!strcmp(keyword,"mtllib")) {
+                    char** split;
+                    size_t split_count;
+                    err = rsge_model_obj_splitStr(line,' ',&split,&split_count);
+                    if(err != RSGE_ERROR_NONE) return err;
+                    size_t matPathSize = strlen(baseDirectory);
+                    if(split[0][0] == '/') split[0]++;
+                    matPathSize += strlen(split[0])+1;
+                    char* matPath = malloc(matPathSize);
+                    if(!matPath) return RSGE_ERROR_MALLOC;
+                    strcpy(matPath,baseDirectory);
+                    strcat(matPath,"/");
+                    strcat(matPath,split[0]);
+                    err = rsge_model_obj_readMaterials(matPath,baseDirectory,&materials);
+                    if(err != RSGE_ERROR_NONE) return err;
+                } else if(!strcmp(keyword,"o")) {
+                    char** split;
+                    size_t split_count;
+                    err = rsge_model_obj_splitStr(line,' ',&split,&split_count);
+                    if(err != RSGE_ERROR_NONE) return err;
+                    currentObjectName = split[0];
+                } else if(!strcmp(keyword,"v")) {
+                    vec3 pos;
+                    err = rsge_model_obj_readVec(line,&pos);
+                    if(err != RSGE_ERROR_NONE) return err;
+                    list_rpush(verts,list_node_new(pos));
+                } else if(!strcmp(keyword,"vn")) {
+                    vec3 pos;
+                    err = rsge_model_obj_readVec(line,&pos);
+                    if(err != RSGE_ERROR_NONE) return err;
+                    list_rpush(normals,list_node_new(pos));
+                } else if(!strcmp(keyword,"vt")) {
+                    vec3 pos;
+                    err = rsge_model_obj_readVec(line,&pos);
+                    if(err != RSGE_ERROR_NONE) return err;
+                    list_rpush(uvs,list_node_new(pos));
+                } else if(!strcmp(keyword,"f")) {
+                } else if(!strcmp(keyword,"usemtl")) {
+                }
+            }
+        }
+        
+        if(nextLine) *nextLine = '\n';
+        line = nextLine ? (nextLine+1) : NULL;
+    }
     return RSGE_ERROR_NONE;
 }
 #endif
